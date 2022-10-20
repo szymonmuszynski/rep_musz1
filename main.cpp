622f67b08e3c2b7d98ad933b3b00990291c545ff
szymonmuszynski <245883@edu.p.lodz.pl>
2022-10-20 23:32:32 +0200

    no message

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+#include <iostream>
+int Policz (int a, int b) {
+    return (a+b)*(a-b);
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout <<"W trzecim branchu; (a+b)*(a-b)=" << Policz(2,3) <<
+              std::endl;
     return 0;
+
 }
